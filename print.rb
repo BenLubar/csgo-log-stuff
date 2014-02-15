4c967a0e444bbad7959c7a478ca63cf2c9ac5527
Ben Lubar <ben.lubar@gmail.com>
2014-02-15 16:51:12 -0600

    move parsing and printing of stats to separate files

---
+++ print.rb
@@ -0,0 +1,19 @@
+require 'sqlite3'
+require 'json'
+
+SQLite3::Database.new 'stats.sqlite3' do |db|
+	stats = {}
+
+	db.query 'select map, sum(t_wins), sum(ct_wins), sum(all_ct_killed), sum(all_t_killed), sum(hostage_reached), sum(hostage_rescued), sum(bomb_planted), sum(bomb_detonated), sum(bomb_defused), sum(time_ran_out) from rounds group by map order by map asc' do |result|
+		result.each_hash do |row|
+			map = stats[row['map']] = {}
+			row.each do |k, v|
+				if k =~ /\Asum\((.*)\)\z/
+					map[$1] = v
+				end
+			end
+		end
+	end
+
+	puts JSON.pretty_generate stats
+end
